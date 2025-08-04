120728fa6525ddd06d9c0ca793415ffac4c2e618
Ian Blas <76878173+ianblas@users.noreply.github.com>
2025-08-04 18:20:37 -0300

    Use lesson slug as Anki deck

--- public/js/anki.js
+++ public/js/anki.js
@@ -14,19 +14,22 @@
     clone.addEventListener('click', () => {
       const lang = localStorage.getItem('lang') || 'es';
       const sanitize = str => str.replace(/[\t\n,]/g, ' ');
+      const basename = location.pathname.split('/').pop().replace(/\.html$/,'');
+      const deck = sanitize(
+        basename.split('-').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join('')
+      );
       const lines = (window.items || [])
         .filter(it => it.term && (it[lang] || it.es))
         .map(it => {
           const term = sanitize(it.term);
           const trans = sanitize(it[lang] || it.es);
-          return `${term},${trans}`;
+          return `${term},${trans},${deck}`;
         })
         .join('\n');
 
       const blob = new Blob([lines], {type:'text/plain;charset=utf-8'});
       const link = document.createElement('a');
       link.href = URL.createObjectURL(blob);
-      const basename = location.pathname.split('/').pop().replace(/\.html$/,'');
       link.download = `${basename}-anki.txt`;
       link.click();
       URL.revokeObjectURL(link.href);
