30ca2ea4038820a6c03e2c3ecba25d02dd83d338
Selene Blas <sele.blas02@gmail.com>
2025-12-29 09:00:54 -0300

    correc23

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,8 +1,6 @@
-const { createClient } = window.supabase;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
     "https://redvymknnxehwveyzmyw.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZHZ5bWtubnhlaHd2ZXl6bXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNzM5NTMsImV4cCI6MjA4MTY0OTk1M30.XcdT2RYq5tKFZziLkOO6mcQZzAUHMJf5ORzkZa2-La8"
 );
-
-window.supabaseInstance = supabase;
